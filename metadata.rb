1292e62f0545cb896030870e06de65a36f8663f0
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-11-18 16:35:33 -0500

    Add source and issues URLs

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures s3_dir'
 long_description 'Installs/Configures s3_dir'
 version          '1.4.1'
 
+source_url 'https://github.com/evertrue/s3_dir/' if respond_to?(:source_url)
+issues_url 'https://github.com/evertrue/s3_dir/issues' if respond_to?(:issues_url)
+
 supports 'ubuntu', '= 14.04'
 
 depends 's3_file', '>= 2.5.0'
