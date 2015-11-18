3639c50b1bee2564b5cddd439bd95d57c3f0e561
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-11-18 16:34:05 -0500

    Add supports value to metadata
    We test on Ubuntu 14.04, so it’s supported. Other OS’s may also work.

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,7 @@ description      'Installs/Configures s3_dir'
 long_description 'Installs/Configures s3_dir'
 version          '1.4.1'
 
+supports 'ubuntu', '= 14.04'
+
 depends 's3_file', '>= 2.5.0'
 depends 'et_fog',  '~> 1.0'
