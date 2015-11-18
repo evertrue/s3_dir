d8c035cdf6abc5953c279d278d03568b70d89bb3
Eric Herot <eric.github@herot.com>
2015-11-18 14:50:59 -0500

    Correctly handle files in subdirectories with no explicitly defined parents directories

--- providers/default.rb
+++ providers/default.rb
@@ -21,28 +21,41 @@ action :create do
     new_resource.dir
   ) if new_resource.mock
 
-  s3_dir_lib.ls(
+  remote_dir_contents = s3_dir_lib.ls(
     new_resource.bucket,
     new_resource.dir
-  ).each do |filename|
-    if filename[-1] == '/'
-      directory "#{new_resource.name}#{filename}" do
-        owner     new_resource.owner
-        group     new_resource.group
-        mode      S3Lib::Dir.dir_mode new_resource.mode
-      end
-    else
-      s3_file "#{new_resource.name}#{filename}" do
-        remote_path "#{new_resource.dir}#{filename}"
-        bucket new_resource.bucket
-        aws_access_key_id new_resource.access_key_id
-        aws_secret_access_key new_resource.secret_access_key
-        owner new_resource.owner
-        group new_resource.group
-        mode new_resource.mode
-        s3_url "#{s3_dir_lib.s3_url}/#{new_resource.bucket}"
-        action :create
-      end
+  )
+
+  # Breaking out the directories here and processing them separately allows
+  # us to avoid creating duplicate resources and gets around the fact that
+  # s3_file cannot create paths recursively.
+  directories = remote_dir_contents.map do |f|
+    f[-1] == '/' ? f.sub(%r{/$}, '') : ::File.dirname(f)
+  end.uniq
+
+  directories.each do |dir|
+    Chef::Log.debug "Processing directory: #{dir}"
+    directory "#{new_resource.name}/#{dir}" do
+      owner     new_resource.owner
+      group     new_resource.group
+      mode      S3Lib::Dir.dir_mode new_resource.mode
+      action    :create
+      recursive true
+    end
+  end
+
+  remote_dir_contents.reject { |f| f[-1] == '/' }.each do |filename|
+    Chef::Log.debug "Processing file: #{filename}"
+    s3_file "#{new_resource.name}/#{filename}" do
+      remote_path "#{new_resource.dir}/#{filename}"
+      bucket new_resource.bucket
+      aws_access_key_id new_resource.access_key_id
+      aws_secret_access_key new_resource.secret_access_key
+      owner new_resource.owner
+      group new_resource.group
+      mode new_resource.mode
+      s3_url "#{s3_dir_lib.s3_url}/#{new_resource.bucket}"
+      action :create
     end
   end
 end
