86d5989d2050ef62d3d2475e78782f51303cf0a8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-01-17 14:35:37 -0500

    Refactor LWRP → Chef 12.5 Custom Resource
    Uses the compat_resource cookbook to maintain backwards compatibility
    to Chef >= 12.1

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,10 @@ version          '1.4.2'
 source_url 'https://github.com/evertrue/s3_dir/' if respond_to?(:source_url)
 issues_url 'https://github.com/evertrue/s3_dir/issues' if respond_to?(:issues_url)
 
+chef_version '>= 12.1'
+
 supports 'ubuntu', '= 14.04'
 
+depends 'compat_resource'
 depends 's3_file', '~> 2.5'
 depends 'et_fog',  '~> 1.0'
--- resources/default.rb
+++ resources/default.rb
@@ -1,15 +1,75 @@
 actions :create
 
-attribute :name,              kind_of: String, name_attribute: true
-attribute :owner,             kind_of: [String],                    default: 'root'
-attribute :group,             kind_of: [String],                    default: 'root'
-attribute :mode,              kind_of: [String, Integer],           default: '0775'
-attribute :bucket,            kind_of: String
-attribute :dir,               kind_of: String,                      default: '/'
-attribute :recursive,         kind_of: [TrueClass, FalseClass],     default: false
-attribute :access_key_id,     kind_of: String
-attribute :secret_access_key, kind_of: String
-attribute :mock,              kind_of: [TrueClass, FalseClass],     default: false
-attribute :region,            kind_of: String,                      default: 'us-east-1'
+property :name,              String, name_attribute: true
+property :owner,             String,                    default: 'root'
+property :group,             String,                    default: 'root'
+property :mode,              [String, Integer],         default: '0775'
+property :bucket,            String
+property :dir,               String,                    default: '/'
+property :recursive,         [TrueClass, FalseClass],   default: false
+property :access_key_id,     String
+property :secret_access_key, String
+property :mock,              [TrueClass, FalseClass],   default: false
+property :region,            String,                    default: 'us-east-1'
 
 default_action :create
+
+action :create do
+  directory new_resource.name do
+    owner     new_resource.owner
+    group     new_resource.group
+    mode      S3Lib::Dir.dir_mode new_resource.mode
+    action    :create
+    recursive new_resource.recursive
+  end
+
+  s3_dir_lib = S3Lib::Dir.new(
+    new_resource.access_key_id,
+    new_resource.secret_access_key,
+    new_resource.region,
+    new_resource.mock
+  )
+
+  s3_dir_lib.build_mock_env(
+    new_resource.bucket,
+    new_resource.dir
+  ) if new_resource.mock
+
+  remote_dir_contents = s3_dir_lib.ls(
+    new_resource.bucket,
+    new_resource.dir
+  )
+
+  # Breaking out the directories here and processing them separately allows
+  # us to avoid creating duplicate resources and gets around the fact that
+  # s3_file cannot create paths recursively.
+  directories = remote_dir_contents.map do |f|
+    f[-1] == '/' ? f.sub(%r{/$}, '') : ::File.dirname(f)
+  end.uniq
+
+  directories.each do |dir|
+    Chef::Log.debug "Processing directory: #{dir}"
+    directory "#{new_resource.name}/#{dir}" do
+      owner     new_resource.owner
+      group     new_resource.group
+      mode      S3Lib::Dir.dir_mode new_resource.mode
+      action    :create
+      recursive true
+    end
+  end
+
+  remote_dir_contents.reject { |f| f[-1] == '/' }.each do |filename|
+    Chef::Log.debug "Processing file: #{filename}"
+    s3_file "#{new_resource.name}/#{filename}" do
+      remote_path "#{new_resource.dir}/#{filename}"
+      bucket new_resource.bucket
+      aws_access_key_id new_resource.access_key_id
+      aws_secret_access_key new_resource.secret_access_key
+      owner new_resource.owner
+      group new_resource.group
+      mode new_resource.mode
+      s3_url "#{s3_dir_lib.s3_url}/#{new_resource.bucket}"
+      action :create
+    end
+  end
+end
